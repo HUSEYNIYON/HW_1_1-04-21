d90591a7625783e10f9b91088371bfb650ada59b
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-04-05 11:58:40 +0500

    Добавьте файлы проекта.

---
+++ QuoteApp/DataContext/QuoteDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuoteApp.DataContext
+{
+    public class QuoteDbContext : DbContext
+    {
+        public QuoteDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected QuoteDbContext()
+        {
+        }
+    }
+}
